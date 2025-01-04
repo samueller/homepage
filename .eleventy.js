7fb73d64cf5665df08101d26e4fdcda42743dd2d
Scott Mueller <scott@scott.am>
2025-01-04 15:52:18 -0800

    Updated 11ty and fixed teaching links

--- .eleventy.js
+++ .eleventy.js
@@ -1,56 +1,18 @@
 const dataFromCMS = (query) =>
-	fetch(process.env.CMS_ENDPOINT, {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json' },
-		body: JSON.stringify({ query })
-	})
-		.then((response) => response.json())
-		.then((json) => json.data)
+    fetch(process.env.CMS_ENDPOINT, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ query })
+    })
+        .then((response) => response.json())
+        .then((json) => json.data)
 
 module.exports = function (config) {
-	config.addPassthroughCopy('src/images')
-	config.addPassthroughCopy('src/styles')
-	config.addPassthroughCopy('src/site.webmanifest')
-	config.addPassthroughCopy('src/favicon.ico')
-	config.addPassthroughCopy('src/*.png')
+    config.addPassthroughCopy('src/images')
+    config.addPassthroughCopy('src/styles')
+    config.addPassthroughCopy('src/site.webmanifest')
+    config.addPassthroughCopy('src/favicon.ico')
+    config.addPassthroughCopy('src/*.png')
 
-	// config.addGlobalData(
-	//   "cms",
-	//   dataFromCMS(`query {
-	// 	basic(where: {id: "clnctyp51hrrk0bmz4xfaxc5r"}) {
-	// 		name
-	// 		longName
-	// 		shortIntro {
-	// 			text
-	// 			html
-	// 		}
-	// 		logo {
-	// 			url
-	// 		}
-	// 		email
-	// 		address
-	// 		whatWeDo {
-	// 			text
-	// 			html
-	// 		}
-	// 		whoWeAre {
-	// 			text
-	// 			html
-	// 		}
-	// 		publications {
-	// 			text
-	// 			html
-	// 		}
-	// 		joinUs {
-	// 			html
-	// 			text
-	// 		}
-	// 		events {
-	// 			text
-	// 			html
-	// 		}
-	// 	}`),
-	// );
-
-	return { dir: { input: 'src' } }
+    return { dir: { input: 'src' } }
 }
